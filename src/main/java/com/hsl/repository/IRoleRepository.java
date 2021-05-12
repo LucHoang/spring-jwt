b630da1125035eeab6d490d646da086eea0d9a34
LucHoang <hoangsyluc@gmail.com>
2021-05-12 10:36:29 +0700

    Thực hành xây dựng JWT

---
+++ src/main/java/com/hsl/repository/IRoleRepository.java
@@ -0,0 +1,10 @@
+package com.hsl.repository;
+
+import com.hsl.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/hsl/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.hsl.service;
+
+import com.hsl.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
+
+public interface IUserService extends IGeneralService<User>, UserDetailsService {
+    Optional<User> findByUsername(String username);
+}
